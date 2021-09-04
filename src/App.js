b4be691e915b42b8d5a405e709c0aa8a45dc9300
Felipe Valencia <fvalencia@vc-soft.com>
2021-09-03 22:39:09 -0500

    Stable, no image analysis, only upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,64 @@
 import './App.css';
-import LabelForm from "./components/LabelForm";
-
+import ImageView from "./components/ImageView";
+import ResponseView from "./components/ResponseView";
+import {useState} from "react";
 /*
 Amplify.configure(awsconfig);
 Amplify.addPluggable(new AmazonAIPredictionsProvider());
-*/
 
+ */
 const App = () => {
+    const [showingField, setShowingField] = useState(true);
+    const [imageURL, setImageURL] = useState('')
+    const [imageName, setImageName] = useState('')
+    const [response, setResponse] = useState([])
+
+    const removeImage = () => {
+        setImageURL('')
+        setImageName('')
+        setShowingField(true)
+    }
+
+    const onChange = (event) => {
+        const {target: {files}} = event;
+        const [file,] = files || [];
+
+        if (!file) {
+            return;
+        }
+
+        setShowingField(false);
+        setImageName(file.name)
+        setImageURL(URL.createObjectURL(file))
+        //predictImage(file);
+    }
+
+    /*const predictImage = (file) => {
+        Predictions.identify({
+            labels: {
+                source: {
+                    file,
+                },
+                type: "LABELS"
+            }
+        }).then(result => {
+            setResponse(result.labels)
+        })
+            .catch(err => console.error(err))
+    }
+
+     */
+
     return (
         <div className="file-upload">
             <h1 style={{textAlign: "center"}} className="header">Upload your image here</h1>
-            <LabelForm/>
+            {showingField ? <div className="image-upload-wrap">
+                <input className="file-upload-input" type='file' onChange={onChange}/>
+                <div className="drag-text">
+                    <h3>Drag and drop a file or select add Image</h3>
+                </div>
+            </div> : <div><ImageView url={imageURL} name={imageName} removeImage={removeImage}/><ResponseView
+                response={response}/></div>}
         </div>
     );
 }
---
+++ src/components/ResponseView.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ResponseView = ({response}) => {
+    return (
+        <></>
+    );
+};
+
+export default ResponseView;
